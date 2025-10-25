06970ea319c847a450a484f74f0c7d26fd7ee09e
Saniya Ahmed <saniyaahmed.jec05@gmail.com>
2025-10-25 17:36:47 +0530

    initial commit for sql_task3

--- sample_dataset/create_inventory_table.sql
+++ sample_dataset/create_inventory_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE inventory (
-    product_id INT IDENTITY(1,1) PRIMARY KEY,   -- Auto increment ID
-    product_name VARCHAR(100) UNIQUE,           -- Unique product
+    product_id INT PRIMARY KEY IDENTITY(1,1),
+    product_name VARCHAR(100),
     category VARCHAR(50),
     quantity INT,
     price DECIMAL(10,2),
